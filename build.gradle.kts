171d0a049b615d035677ed1acf3a885fee5ba13a
Abhishek Mishra <abhiabhishek2714@gmail.com>
2026-06-08 00:25:26 +0530

    onlide postgreSQL added

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,8 @@ repositories {
 }
 
 dependencies {
-
+	
+    implementation("me.paulschwarz:spring-dotenv:4.0.0")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
